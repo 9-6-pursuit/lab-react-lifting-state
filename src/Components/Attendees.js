0efdc58f57e59e7226e8c043bf0916325dc52b8b
crsvalle <cristian.valle@pursuit.org>
2023-04-26 22:02:59 -0400

    headache

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,18 @@
-export default function Attendees() {
-  return;
+
+import Attendee from "./Attendee";
+
+export default function Attendees({attendees, event, updateEventAttendance}) {
+  
+  return(
+    <div className="attendees">
+      {attendees.map((attendee, index) => (
+          <Attendee
+            event={event}
+            attendee={attendee}
+            updateEventAttendance={updateEventAttendance}
+          />
+      ))}
+    </div>
+  )
+  ;
 }
