ac4fef12aef29a39ac57e7fe692df4d205b5c088
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-04-27 11:51:29 -0400

    lab react lifting state

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,23 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({event, toggleEventAttendees, showAttendees, attendees, updateEventAttendance}) {
+  return (
+    <>
+      <button onClick={toggleEventAttendees}>
+        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+      </button>
+
+      {showAttendees ? (
+        <div className="attendees">
+          {attendees.map((attendee, index) => (
+            <Attendee
+            event={event}
+            attendee={attendee}
+            updateEventAttendance={updateEventAttendance}
+            />
+          ))}
+        </div>
+      ) : null}
+    </>
+  )
 }
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,24 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({event, toggleEventAttendees, showAttendees, attendees,updateEventAttendance}) {
+  return (
+    <>
+      <li key={event.id}>
+        <img src={event.eventImage} alt={event.name} />
+        <h5>
+          {event.name} {event.eventType}
+        </h5>
+        <br />
+        <span>Organized by: {event.organizer} </span>
+        <br />
+        <Attendees
+        event = {event}
+        toggleEventAttendees={toggleEventAttendees}
+        showAttendees={showAttendees}
+        attendees={attendees}
+        updateEventAttendance={updateEventAttendance}
+        />
+      </li>
+    </>
+  )
 }
