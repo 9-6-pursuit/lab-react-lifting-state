f9d4c5cde96b442a2f91f3eaa4591deff25452b1
AllahvelS <allahvelsalisbury@pursuit.org>
2023-04-27 00:24:29 -0400

    Complete.

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,11 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee"
+
+export default function Attendees({event, attendee, updateEventAttendance}) {
+  return (
+    <Attendee 
+    attendee={attendee} 
+    event={event} 
+    updateEventAttendance={updateEventAttendance}
+    />
+  );
 }
