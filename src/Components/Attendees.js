5ca6a9a24afff8b083114fd0d72ac3d4e650285d
“irwinj7” <Irwinjorge@pursuit.com>
2023-04-26 21:20:15 -0400

    good progress

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,6 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee.js"
+export default function Attendees({attendee,event,updateEventAttendance}) {
+  return (
+    <Attendee attendee={attendee} event={event} updateEventAttendance={updateEventAttendance}/>
+  )
 }
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,29 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees.js"
+export default function Event({event,toggleEventAttendees,showAttendees,attendees,updateEventAttendance}) {
+  return (
+  <>
+    <li key={event.id}>
+      <img src={event.eventImage} alt={event.name} />
+      <h5>
+        {event.name} {event.eventType}
+      </h5>
+      <br />
+      <span>Organized by: {event.organizer} </span>
+      <br />
+      <>
+        <button onClick={toggleEventAttendees}>
+          {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+        </button>
+
+        {showAttendees ? (
+          <div className="attendees">
+            {attendees.map((attendee, index) => (
+              <Attendees event={event} toggleEventAttendees={toggleEventAttendees} showAttendees={showAttendees} attendees={attendees} updateEventAttendance={updateEventAttendance}/>
+            ))}
+          </div>
+        ) : null}
+      </>
+    </li>
+  </>
+  )
 }
