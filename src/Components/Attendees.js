84705742d11d70b8565fb363a4a63b60d6ce6820
Sabri Mohiuddin <you@example.com>
2023-04-26 23:18:17 -0400

    🔬 React Lifting State Lab

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,12 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({attendee,event,updateEventAttendance}) {
+  return (
+    <Attendee 
+    attendee={attendee} 
+    event={event} 
+    updateEventAttendance={updateEventAttendance}
+    />
+  );
 }
+
