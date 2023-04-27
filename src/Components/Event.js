5b0575557315a2b66d05b0dc54c5d65749256935
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-04-26 21:12:40 -0400

    4/5

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,8 +1,16 @@
-import React from "react";
 
-export default function Event({event, attendees, showAttendees, toggleEventAttendees, updateEventAttendance}) {
+import Attendees from "./Attendees";
+import React, { useState } from 'react';
+
+
+export default function Event({ event, attendees, updateEventAttendance }) {
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  const toggleEventAttendees = () => {
+    setShowAttendees(!showAttendees);
+  };
+
   return (
-    <>
     <li key={event.id}>
       <img src={event.eventImage} alt={event.name} />
       <h5>
@@ -11,54 +19,12 @@ export default function Event({event, attendees, showAttendees, toggleEventAtten
       <br />
       <span>Organized by: {event.organizer} </span>
       <br />
-      <>
-        <button onClick={toggleEventAttendees}>
-          {!showAttendees ? "Show Attendees" : "Hide Attendees"}
-        </button>
-
-        {showAttendees ? (
-          <div className="attendees">
-            {attendees.map((attendee, index) => (
-              <>
-                <div key={attendee.id} className="attendee">
-                  <p>
-                    <img
-                      src={attendee.avatar}
-                      alt={attendee.firstName}
-                    />
-                    {"   "}
-                    <span>
-                      {" "}
-                      {attendee.firstName} {attendee.lastName}{" "}
-                    </span>
-                  </p>
-                  <p>
-                    <button
-                      className="clickable"
-                      onClick={() =>
-                        updateEventAttendance(
-                          event.id,
-                          attendee.id
-                        )
-                      }
-                    >
-                      Attending:
-                    </button>
-                    <span>
-                      {attendee.attendance ? "✅" : "❌"}
-                    </span>
-                  </p>
-
-                  <p>
-                    <span>Note:</span> {attendee.note}
-                  </p>
-                </div>
-              </>
-            ))}
-          </div>
-        ) : null}
-      </>
+      <Attendees
+        attendees={attendees}
+        showAttendees={showAttendees}
+        toggleEventAttendees={toggleEventAttendees}
+        updateEventAttendance={updateEventAttendance}
+      />
     </li>
-  </>
   );
 }
