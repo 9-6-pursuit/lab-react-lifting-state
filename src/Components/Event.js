7df43a6b62e5c6c1573d1d983548818cb93e7b62
Judy <judychue@pursuit.org>
2023-04-27 22:25:19 -0400

    passed

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,43 @@
-export default function Event() {
-  return;
+// #2
+import React from "react";
+import Attendees from "./Attendees";
+import { useState } from "react";
+
+export default function Event({event, attendees, updateEventAttendance}) {
+  const [showAttendees, setShowAttendees] = useState(false);
+ 
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+  
+  return (
+    <>
+    <li key={event.id}>
+      <img src={event.eventImage} alt={event.name} />
+      <h5>
+        {event.name} {event.eventType}
+      </h5>
+      <br />
+      <span>Organized by: {event.organizer} </span>
+      <br />
+      <>
+        <button onClick={toggleEventAttendees}>
+          {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+        </button>
+
+        {showAttendees ? (
+          <div className="attendees">
+            {attendees.map((attendee, index) => (
+                <Attendees 
+                attendee={attendee} 
+                event={event} 
+                updateEventAttendance={updateEventAttendance}
+                />
+            ))}
+          </div>
+        ) : null}
+      </>
+    </li>
+  </>
+  );
 }
