2112326f2f0038c780f96e717aec173d835b5839
Lakisha J <lakishajohnson@pursuit.org>
2023-04-27 20:55:59 -0400

    Imported Attendees component

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,12 +1,7 @@
 import React from "react";
+import Attendees from "./Attendees";
 
-export default function Event({
-  event,
-  showAttendees,
-  attendees,
-  toggleEventAttendees,
-  updateEventAttendance,
-}) {
+export default function Event({ event, showAttendees, attendees, toggleEventAttendees, updateEventAttendance }) {
   return (
     <>
       <li className="event" key={event.id}>
@@ -17,45 +12,13 @@ export default function Event({
         <br />
         <span>Organized by: {event.organizer} </span>
         <br />
-        <>
-          <button onClick={() => toggleEventAttendees(event.id)}>
-            {!showAttendees[event.id] ? "Show Attendees" : "Hide Attendees"}
-          </button>
-
-          {showAttendees[event.id] ? (
-            <div className="attendees">
-              {attendees.map((attendee, index) => (
-                <>
-                  <div key={attendee.id} className="attendee">
-                    <p>
-                      <img src={attendee.avatar} alt={attendee.firstName} />
-                      {"   "}
-                      <span>
-                        {" "}
-                        {attendee.firstName} {attendee.lastName}{" "}
-                      </span>
-                    </p>
-                    <p>
-                      <button
-                        className="clickable"
-                        onClick={() =>
-                          updateEventAttendance(event.id, attendee.id)
-                        }
-                      >
-                        Attending:
-                      </button>
-                      <span>{attendee.attendance ? "✅" : "❌"}</span>
-                    </p>
-
-                    <p>
-                      <span>Note:</span> {attendee.note}
-                    </p>
-                  </div>
-                </>
-              ))}
-            </div>
-          ) : null}
-        </>
+      <Attendees
+        showAttendees={showAttendees}
+        attendees={attendees}
+        toggleEventAttendees={toggleEventAttendees}
+        event={event}
+        updateEventAttendance={updateEventAttendance}
+      />
       </li>
     </>
   );
