86f79d4cab2b9472e581db5a72ccc6671d1f9c21
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-04-27 22:33:46 -0400

    Moved code to files

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,24 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({event, attendees, showAttendees, toggleEventAttendees, updateEventAttendance}) {
+  return (
+    <>
+      <li key={event.id}>
+        <img src={event.eventImage} alt={event.name} />
+        <h5>
+          {event.name} {event.eventType}
+        </h5>
+        <br />
+        <span>Organized by: {event.organizer} </span>
+        <br />
+        <Attendees 
+          event={event}
+          attendees={attendees}
+          showAttendees={showAttendees}
+          toggleEventAttendees={toggleEventAttendees}
+          updateEventAttendance={updateEventAttendance}
+        />
+      </li>
+    </>
+  );
 }
