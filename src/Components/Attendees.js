f60358a3b71215fc858322f1cbf76dd9dd5a5398
Lakisha J <lakishajohnson@pursuit.org>
2023-04-27 20:59:23 -0400

    Imports Attendee

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,30 @@
-export default function Attendees() {
-  return;
+import React from "react";
+import Attendee from "./Attendee";
+
+export default function Attendees({
+  showAttendees,
+  attendees,
+  toggleEventAttendees,
+  event,
+  updateEventAttendance,
+}) {
+  return (
+    <>
+      <button onClick={() => toggleEventAttendees(event.id)}>
+        {!showAttendees[event.id] ? "Show Attendees" : "Hide Attendees"}
+      </button>
+      {showAttendees[event.id] ? (
+        <div className="attendees">
+          {attendees.map((attendee, index) => (
+            <Attendee
+              key={attendee.id}
+              attendee={attendee}
+              event={event}
+              updateEventAttendance={updateEventAttendance}
+            />
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
 }
