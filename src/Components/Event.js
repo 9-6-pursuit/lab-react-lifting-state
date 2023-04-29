cd2f5a43724d00e83dc99d82ebc0ed2addf1534a
KleemoffDeveloper <kleemoffdeveloper@gmail.com>
2023-04-29 14:34:41 -0400

    cypress passing

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,39 @@
-export default function Event() {
-  return;
+import { useState } from "react";
+
+import Attendee from "./Attendee";
+import Attendees from "./Attendees";
+
+export default function Event({ event, attendees, updateEventAttendance }) {
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+
+  return (
+    <li key={event.id} className="event">
+      <img src={event.eventImage} alt={event.name} />
+      <h5>
+        {event.name} {event.eventType}
+      </h5>
+      <br />
+      <span>Organized by: {event.organizer} </span>
+      <br />
+      <Attendees
+        toggleEventAttendees={toggleEventAttendees}
+        showAttendees={showAttendees}
+      />
+      {showAttendees ? (
+        <div className="attendees">
+          {attendees.map((attendee) => (
+            <Attendee
+              event={event}
+              attendee={attendee}
+              updateEventAttendance={updateEventAttendance}
+            />
+          ))}
+        </div>
+      ) : null}
+    </li>
+  );
 }
