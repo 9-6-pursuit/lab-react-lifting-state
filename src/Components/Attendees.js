327c4984bfbbfbf1a8f08de46a4765c708d369eb
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-04-29 20:38:29 -0400

    Completed

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,32 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+import { useState } from "react";
+
+export default function Attendees({event, attendees, updateEventAttendance}) {
+  
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+  
+  
+  return (
+    <>
+    <button onClick={toggleEventAttendees}>
+      {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+    </button>
+
+    {showAttendees ? (
+      <div className="attendees">
+        {attendees.map((attendee, index) => (
+           <Attendee 
+           attendee = {attendee}
+           updateEventAttendance={updateEventAttendance}
+           event={event}
+           /> 
+        ))}
+      </div>
+    ) : null}
+  </>
+  )
 }
