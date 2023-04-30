7d2f7c1d02602edc6d97785c0c2016d5cb7a43df
AlejandraRV91 <alejandraramirez@pursuit.org>
2023-04-30 10:28:59 -0500

    lab lifting DONE

--- src/Components/Attendee.js
+++ src/Components/Attendee.js
@@ -1,3 +1,51 @@
-export default function Attendee() {
-  return;
+/** @format */
+
+export default function Attendee(props) {
+	function updateEventAttendance(eventId, attendeeId) {
+		const eventArray = [...props.events];
+		const eventIndex = eventArray.findIndex(
+			(event) => eventId === event.id
+		);
+		const event = { ...eventArray[eventIndex] };
+		const personIndex = event.people.findIndex(
+			(person) => person.id === attendeeId
+		);
+		const peopleArray = [...props.event.people];
+		peopleArray[personIndex].attendance =
+			!peopleArray[personIndex].attendance;
+		event.people = peopleArray;
+		eventArray[eventIndex] = event;
+		props.setEvents(eventArray);
+	}
+	return (
+		<>
+			<div key={props.attendee.id} className="attendee">
+				<p>
+					<img
+						src={props.attendee.avatar}
+						alt={props.attendee.firstName}
+					/>
+					<span>
+						{props.attendee.firstName} {props.attendee.lastName}
+					</span>
+				</p>
+				<p>
+					<button
+						className="clickable"
+						onClick={() =>
+							updateEventAttendance(
+								props.event.id,
+								props.attendee.id
+							)
+						}>
+						Attending:
+					</button>
+					<span>{props.attendee.attendance ? "✅" : "❌"}</span>
+				</p>
+				<p>
+					<span>Note:</span> {props.attendee.note}
+				</p>
+			</div>
+		</>
+	);
 }
--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,37 @@
-export default function Attendees() {
-  return;
+/** @format */
+
+import { useState } from "react";
+import Attendee from "./Attendee";
+
+export default function Attendees(props) {
+	let [showAttendees, setShowAttendees] = useState(false);
+
+	function toggleEventAttendees() {
+		if (showAttendees) {
+			setShowAttendees(false);
+		} else {
+			setShowAttendees(true);
+		}
+	}
+
+	return (
+		<>
+			<button onClick={toggleEventAttendees}>
+				{!showAttendees ? "Show Attendees" : "Hide Attendees"}
+			</button>
+			{showAttendees ? (
+				<div className="attendees">
+					{props.attendees.map((attendee, index) => {
+						return (
+							<Attendee
+								attendee={attendee}
+								event={props.event}
+								events={props.events}
+								setEvents={props.setEvents}></Attendee>
+						);
+					})}
+				</div>
+			) : null}
+		</>
+	);
 }
