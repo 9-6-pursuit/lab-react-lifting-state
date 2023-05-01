cae14b8983855df9cda80b8d2ee078e2ef1f5f18
Monir Hossen <monirhossen@pursuit.org>
2023-05-01 19:39:18 -0400

    Bug fixed

---
+++ src/Attendees.js
@@ -0,0 +1,35 @@
+import { event } from "cypress/types/jquery";
+import Attendee from "./Components/Attendee";
+import { useState } from "react";
+
+export default function Attendees({event, attendees, updateEventAttendance}){
+
+  const [showAttendees, setShowAttendees] = useState(false)
+
+  function toggleEventAttendees (){
+    setShowAttendees(!showAttendees)
+  }
+    
+    return (
+<>
+                      <button onClick={toggleEventAttendees}>
+                        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+                      </button>
+
+                      {showAttendees ? (
+                        <div className="attendees">
+                          {attendees.map((attendee, index) => (
+                            
+                              <Attendee 
+                              attendee ={attendee}
+                              event ={event}
+                              updateEventAttendance ={updateEventAttendance}
+                              />
+                      
+                          ))}
+                        </div>
+                      ) : null}
+                    </>
+
+    )
+}
