4d0434c15a18e24a8203f48b964c3fbeddddcc7c
Monir Hossen <monirhossen@pursuit.org>
2023-05-01 10:46:14 -0400

    added more components

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer (){
+    return (<>
+        <footer>
+          <ul>
+            <li>Contact</li>
+            <li>About</li>
+            <li>Legal</li>
+          </ul>
+        </footer>
+      </>)
+}
