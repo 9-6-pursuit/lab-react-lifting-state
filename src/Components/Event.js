ef578a85326cd5c8440523e74bd72c04bb11d118
Monir Hossen <monirhossen@pursuit.org>
2023-05-02 17:38:24 -0400

    Files were misplaced

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,27 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees"
+export default function Event ({event, attendees, updateEventAttendance, showAttendees, toggleEventAttendees}){
+    return (
+        <>
+                  <li key={event.id}>
+                    <img src={event.eventImage} alt={event.name} />
+                    <h5>
+                      {event.name} {event.eventType}
+                    </h5>
+                    <br />
+                    <span>Organized by: {event.organizer} </span>
+                    <br />
+
+                    <Attendees 
+                    event ={event}
+                    attendees ={attendees}
+                    updateEventAttendance ={updateEventAttendance}
+                    showAttendees ={showAttendees}
+                    toggleEventAttendees ={toggleEventAttendees}
+
+              
+                    />
+                    
+                  </li>
+                </>
+    )
 }
