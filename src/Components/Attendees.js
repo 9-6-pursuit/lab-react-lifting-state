8194c418f9caa390c7221b59ebcd6640de11d064
fintheehuman <finessepoole@pursuit.org>
2023-06-15 01:59:26 -0400

    yuh.

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,20 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({toggleEventAttendees, showAttendees, attendees, updateEventAttendance, event}) {
+  return(
+    <>
+                      <button onClick={toggleEventAttendees}>
+                        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+                      </button>
+
+                      {showAttendees ? (
+                        <div className="attendees">
+                          {attendees.map((attendee, index) => (
+                            <Attendee attendee={attendee} updateEventAttendance={updateEventAttendance} event={event}/>
+                          ))}
+                        </div>
+                      ) : null}
+                    </>
+  );
 }
+
